a3a444530d6d9c62adcf8ce1c290b8496a17f5a3
Mark Zsombor <markszsombor@gmail.com>
2018-11-25 21:03:16 -0800

    created launch page with list of topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,6 +4,11 @@ defmodule Discuss.TopicController do
   # alias allows for Topic to stand in for Discuss.Topic
   alias Discuss.Topic
 
+  def index(conn, _params) do 
+    topics = Repo.all(Topic)
+    # render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
@@ -12,7 +17,8 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} -> 
+        render conn, "index.html"
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
     end
