4d0bf9b7adebc424460ad4cf27e131c15669bb88
Mark Zsombor <markszsombor@gmail.com>
2018-11-25 20:11:40 -0800

    create a form for making new topics, piped it the DB

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,9 +4,16 @@ defmodule Discuss.TopicController do
   # alias allows for Topic to stand in for Discuss.Topic
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
+  end
 end
